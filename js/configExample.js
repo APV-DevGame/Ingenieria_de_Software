ef09af672fc9f330d8541a9d885933067493e3f9
Irvin <96852240+irvingm45@users.noreply.github.com>
2025-11-08 22:38:55 -0600

    Se conecto la base de datos al login.html en las líneas 40-44 procurar seguir el ejemplo "configExample.js" pero con credenciales reales y con lo especificado en dicho ejemplo

---
+++ js/configExample.js
@@ -0,0 +1,6 @@
+// Añade tu propio archivo config.js con tus credenciales reales
+// Asegurate que el archivo se llame "config.js"
+const CONFIG = {
+    SUPABASE_URL: "https://urlDeLaBase.supabase.co",
+    SUPABASE_KEY: "fdkajujeioqrueqioprewjncbcxvhjjfhues"
+};
