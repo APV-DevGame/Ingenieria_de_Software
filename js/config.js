e0965f586a9f946c98d23ecbeec5374c599be8ad
Irvin <96852240+irvingm45@users.noreply.github.com>
2025-12-04 19:22:31 -0600

    Se agrego el config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    SUPABASE_URL: "https://thfkmwzpnwpecjtpklir.supabase.co",
+    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRoZmttd3pwbndwZWNqdHBrbGlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1NzIxNTgsImV4cCI6MjA3NDE0ODE1OH0.mD1c-a-coljvKlIv7Hwi1H3gdeoJMQbJFrkgtKJWQ5g"
+};
